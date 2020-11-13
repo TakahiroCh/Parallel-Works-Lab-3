156e6680c1b42c57fe082ce689cd28bc130ace95
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 15:31:10 +0300

    Scripted auto-commit on change (2020-11-13 15:31:10) by gitwatch.sh

--- src/main/java/ru/Ivan/Test.java
+++ src/main/java/ru/Ivan/Test.java
@@ -1,5 +1,34 @@
 package ru.Ivan;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class Test {
-    
+    private final String TEST_NAME = "testName";
+    private final String EXPECTED_RESULT = "expectedResult";
+    private final String PARAMS = "params";
+    private final String RESULT = "result";
+
+    @JsonProperty(TEST_NAME)
+    private String testName;
+
+    @JsonProperty(EXPECTED_RESULT)
+    private String expectedResult;
+
+    @JsonProperty(PARAMS)
+    private ArrayList<Integer> params;
+
+    @JsonProperty(RESULT)
+    private boolean result;
+
+    @JsonCreator
+    public Test(@JsonProperty(TEST_NAME) String testName, @JsonProperty(EXPECTED_RESULT) expectedResult,
+            @JsonProperty(PARAMS) ArrayList<Integer> params, @JsonProperty(RESULT) boolean result) {
+        
+    }
+
+
+
 }
