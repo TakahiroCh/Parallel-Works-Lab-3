069cbd5c309eef6a5d30b39fb5c887a79ca8e020
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 16:36:54 +0300

    Scripted auto-commit on change (2020-11-13 16:36:54) by gitwatch.sh

--- src/main/java/ru/Ivan/TestActor.java
+++ src/main/java/ru/Ivan/TestActor.java
@@ -1,7 +1,12 @@
 package ru.Ivan;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestActor extends AbstractActor {
-    
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match()
 }
