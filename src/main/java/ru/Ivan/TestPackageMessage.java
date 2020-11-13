58a84112cd90f5029fc950755ca6e424d0212143
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 21:09:27 +0300

    Scripted auto-commit on change (2020-11-13 21:09:27) by gitwatch.sh

--- src/main/java/ru/Ivan/TestPackageMessage.java
+++ src/main/java/ru/Ivan/TestPackageMessage.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class TestPackageMessage {
     private final String PACKAGE_ID = "packageId";
     private final String JS_SCRIPT = "jsScript";
-    private final String FUNCTION_NAME = "packageId";
+    private final String FUNCTION_NAME = "functionName";
     private final String TEST = "test";
 
     @JsonProperty(PACKAGE_ID)
