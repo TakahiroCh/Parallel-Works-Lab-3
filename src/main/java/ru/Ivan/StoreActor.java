e9cb1345d5b8150fbc8e823bd4a750aeda3772fb
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 15:06:31 +0300

    Scripted auto-commit on change (2020-11-13 15:06:31) by gitwatch.sh

--- src/main/java/ru/Ivan/StoreActor.java
+++ src/main/java/ru/Ivan/StoreActor.java
@@ -4,11 +4,13 @@ import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class StoreActor extends AbstractActor {
-    private HashMap<Integer, ArrayList<String>> storage = new HashMap<Integer, ArrayList<String>>();
+    private Map<Integer, ArrayList<String>> storage = new HashMap<Integer, ArrayList<String>>();
 
     
+
 
 
 }
