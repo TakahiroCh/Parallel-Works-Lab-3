838198530c763c983699404771c7fd3c36c37a99
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 15:06:53 +0300

    Scripted auto-commit on change (2020-11-13 15:06:53) by gitwatch.sh

--- src/main/java/ru/Ivan/StoreActor.java
+++ src/main/java/ru/Ivan/StoreActor.java
@@ -10,7 +10,7 @@ public class StoreActor extends AbstractActor {
     private Map<Integer, ArrayList<String>> storage = new HashMap<Integer, ArrayList<String>>();
 
     @Override
-    public 
+    public Receive 
 
 
 
