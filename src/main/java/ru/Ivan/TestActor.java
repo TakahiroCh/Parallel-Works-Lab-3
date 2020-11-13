85a316fb895d1fa71e19b33476935302b8dfc679
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 18:00:19 +0300

    Scripted auto-commit on change (2020-11-13 18:00:19) by gitwatch.sh

--- src/main/java/ru/Ivan/TestActor.java
+++ src/main/java/ru/Ivan/TestActor.java
@@ -5,13 +5,22 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+
 public class TestActor extends AbstractActor {
-    private ActorSelection storeActor = ActorSystem("/user/takahiro/storeActor");
+    private ActorSelection storeActor = ActorSystem.apply().actorSelection("/user/takahiro/storeActor");
+
+    private ArrayList<Test> runTest() {
+
+    }
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestMessage.class, m -> {
-                storeActor.tell()}
+                    storeActor.tell(new StoreMessage(m.getPackageId,
+                            runTest()
+                    ));
+                }
     })
 }
