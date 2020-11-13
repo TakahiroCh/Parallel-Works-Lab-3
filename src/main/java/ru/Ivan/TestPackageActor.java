0a39af04d8185388c168d58e18617d6e2b6f9b83
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 21:06:06 +0300

    Scripted auto-commit on change (2020-11-13 21:06:06) by gitwatch.sh

--- src/main/java/ru/Ivan/TestPackageActor.java
+++ src/main/java/ru/Ivan/TestPackageActor.java
@@ -1,6 +1,21 @@
 package ru.Ivan;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TestPackageActor extends AbstractActor {
+    private ActorSelection testPerformerRouter = ActorSystem.apply().actorSelection("/user/takahiro/testPerformerActor");
+
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(TestPackageMessage.class, m -> {
+                    for (Test test: m.getTests()) {
+                        testPerformerRouter
+                                .tell(new TestMessage(m.getPackageId(), m.getJsScript(), m.getFunctionName, test), self());
+                    }
+                })
+                .build();
+    }
 }
