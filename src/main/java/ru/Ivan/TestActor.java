c8d7bb3284be09bde13ed120e2e04e31674887d0
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 18:11:33 +0300

    Scripted auto-commit on change (2020-11-13 18:11:33) by gitwatch.sh

--- src/main/java/ru/Ivan/TestActor.java
+++ src/main/java/ru/Ivan/TestActor.java
@@ -5,14 +5,22 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.ArrayList;
 
 public class TestActor extends AbstractActor {
     private ActorSelection storeActor = ActorSystem.apply().actorSelection("/user/takahiro/storeActor");
+    private final String SCRIPT_BY_NAME = "nashorn";
 
     private ArrayList<Test> runTest(String jsScript, String functionName, String testName,
-                                    String expectedResult, ArrayList<Integer> params) {
-        
+                                    String expectedResult, ArrayList<Integer> params) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(SCRIPT_BY_NAME);
+        engine.eval(jsScript);
+        Invocable invocable = (Invocable) engine;
+        String result = invocable.invokeFunction(functionName, params.toArray()).toString();
 
 
     }
