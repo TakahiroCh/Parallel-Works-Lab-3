8fe9c4576d5199853fd515f26be5d96a51f35cf2
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-11-13 15:36:03 +0300

    Scripted auto-commit on change (2020-11-13 15:36:03) by gitwatch.sh

--- src/main/java/ru/Ivan/StoreActor.java
+++ src/main/java/ru/Ivan/StoreActor.java
@@ -8,13 +8,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StoreActor extends AbstractActor {
-    private Map<Integer, ArrayList<Test>> store = new HashMap<Integer, ArrayList<String>>();
+    private Map<Integer, ArrayList<Test>> store = new HashMap<Integer, ArrayList<Test>>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder().create()
                 .match(StoreMessage.class, m -> {
-                    if (store.containsKey(m.))
+                    if (store.containsKey(m.get))
                     store.put()
                 })
     }
